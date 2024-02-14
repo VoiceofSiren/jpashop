c6fc5dc1d6288ae656d620ede7e43519c9b33b89
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-02-14 15:31:25 +0900

    done: Entity classes were defined and relationships between tables are mapped using JPA

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.item.CategoryItem;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<CategoryItem> itemCategories = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children = new ArrayList<>();
+}
