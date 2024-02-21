80d6aad826bd814a52c12b8bd42a3ecd8ba336f3
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-02-21 16:16:30 +0900

    new: orderRepository and orderService

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public Long save(Order order) {
+        em.persist(order);
+        return order.getId();
+    }
+
+    public Order findOne(Long orderId) {
+        return em.find(Order.class, orderId);
+    }
+
+
+}
