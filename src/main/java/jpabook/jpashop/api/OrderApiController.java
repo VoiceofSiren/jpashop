6571a46399ae3268c835b13094442186051f959c
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-02-28 13:22:38 +0900

    new: v3.1

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -44,6 +45,15 @@ public class OrderApiController {
     @GetMapping("/api/v3/orders")
     public List<OrderDTO> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
+        return orders.stream()
+                .map(order -> new OrderDTO(order))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDTO> ordersV3_paging(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                                          @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
         return orders.stream()
                 .map(order -> new OrderDTO(order))
                 .collect(Collectors.toList());
